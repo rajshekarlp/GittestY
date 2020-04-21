90cba0ac81ed76b4b12a438ff42bda8f1be12c77
rajshekar <rajshekar.lp@gmail.com>
2020-04-21 12:48:31 +0530

    eeee

--- src/test/java/qaclickacademy/com/seleniumTest.java
+++ src/test/java/qaclickacademy/com/seleniumTest.java
@@ -5,9 +5,17 @@ import org.testng.annotations.Test;
 public class seleniumTest {
 	
 	@Test
+	public void browserAutomation123()
+	{
+		System.out.println("browserAutomation1");
+		System.out.println("browserAutomation2");
+		System.out.println("browserAutomation3");
+	}
+	@Test
 	public void browserAutomation()
 	{
 		System.out.println("browserAutomation");
+		
 	}
 
 	@Test
